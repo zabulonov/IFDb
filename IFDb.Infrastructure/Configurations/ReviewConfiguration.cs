0e19385dea4063b685b003f49278eee7061b040e
Alexey Zabulonov <zabulonov444@yandex.ru>
2023-05-01 11:00:26 +0300

    Add Configurations for Entities

---
+++ IFDb.Infrastructure/Configurations/ReviewConfiguration.cs
@@ -0,0 +1,20 @@
+using Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IFDb.Infrastructure.Configurations;
+
+public class ReviewConfiguration : IEntityTypeConfiguration<Review>
+{
+    public void Configure(EntityTypeBuilder<Review> builder)
+    {
+        builder.ToTable(nameof(Review));
+                
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).ValueGeneratedOnAdd();
+        
+        builder.Property(x => x.Creator).IsRequired();
+        builder.Property(x => x.Grade).IsRequired();
+        builder.Property(x => x.Text).IsRequired();
+    }
+}
